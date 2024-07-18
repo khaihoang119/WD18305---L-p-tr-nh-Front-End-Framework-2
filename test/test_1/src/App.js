811f73bcad1e7b7d29b4fb893ae4ecf8fd5111be
KhaiHoangSieuCoder <60562319+khaihoang119@users.noreply.github.com>
2024-07-18 17:57:20 +0700

    login vali

--- test/test_1/src/App.js
+++ test/test_1/src/App.js
@@ -1,152 +1,32 @@
-
-// import React, { useEffect, useState } from 'react';
+import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-// import StudentList from './components/Studentinfo/studentList';
-// import StudentInfo1 from './components/Studentinfo/Student';
+import { Button, FormText, FormCheck } from 'react-bootstrap';
+import {
+RouterProvider,
+Route,
+createBrowserRouter,
+createRoutesFromElements,
+} from 'react-router-dom'
+
 import Login from './pages/Login';
 import Register from './pages/Register';
-import{
-  RouterProvider,
-  Route,
-  createBrowserRouter,
-  createRoutesFromElements
-
-} from 'react-router-dom'
 
 function App() {
   const router = createBrowserRouter(
     createRoutesFromElements(
       <Route>
-        <Route path="/login" element={<Login/>} />
-        <Route path="/register" element={<Register/>} />
+        <Route path='/login' element={<Login/>}/>
+        <Route path='/register' element={<Register/>}/>
       </Route>
     )
-  );
-  
-  // const students = [
-  //   {
-  //     name: "Nguyễn Khải Hoàng",
-  //     code: "PC07159",
-  //     address: "Can Tho City"
-  //   },
-  //   {
-  //     name: "Trần Hữu Đạt",
-  //     code: "PC07116",
-  //     address: "Can Tho City"
-  //   },
-  //   {
-  //     name: "Lý Thiên Bảo",
-  //     code: "PC07117",
-  //     address: "Can Tho City"
-  //   },
-  //   {
-  //     name: "Nguyễn Thị Phương Thư",
-  //     code: "PC07032",
-  //     address: "An Giang City"
-  //   },
-  //   {
-  //     name: "Lê Thị Kim Yến",
-  //     code: "PC06995",
-  //     address: "Can Tho City"
-  //   }
+  )
 
-  // ];
-
-  // useEffect(() => {
-  //   console.log("render success");
-  // }, []);
-
-  // const [student, setStudent] = useState({
-  //   name: "Nguyen Van A",
-  //   code: "PC07159",
-  //   address: "Can Tho City"
-  // });
-  // const [count, setCount] = useState(0)
-  // const minus = () =>{
-  //   setCount(count - 1 )
-  // }
-  // const plus = () =>{
-  //   console.log("-------");
-  //   console.log("count == ",count);
-  //   const countTemp = count + 1;
-  //   setCount(countTemp);
-  //   console.log("count === ",countTemp);
-  // }
-  //viet state => diem sv ==0
-  //if diem < 5 =>fail
-  //if die4m > 5 =>pass
-
-  // const [point, setPoint] = useState(0);
-  // const minusPoint = () =>{
-  //   if(point > 0){
-  //     setPoint(point - 1)
-  //   }
-  // }
-  // const plusPoint = () =>{
-  //   if(point < 10){
-  //     setPoint(point + 1)
-  //   }
-  // }
-
-  // const changeName = () => {
-  //   // let studentTemp = student;
-  //   // studentTemp.name = "Nguyen Van B";
-  //   // setStudent(studentTemp);
-  //   setStudent({...student, name: "Nguyen Van B"});
-  // }
   return (
     <>
-    <RouterProvider router={router} />
+    <RouterProvider router={router}/>
     </>
-    // <div className="container">
-    //   <h1>test 3</h1>
-    //   <StudentInfo1 name={student.name} code={student.code} address={student.address} />
-    //   <button className='btn btn-primary' onClick={changeName}>{'Change Name'}</button>
-    //   <h1>Lab 2 bài 3</h1>
-    //   <h1>Danh sách sinh viên nhóm 5 AE</h1>
-    //   <table className="table table-striped">
-    //     <thead>
-    //       <tr class="table-primary">
-    //         <th scope="col">#</th>
-    //         <th scope="col">Họ và Tên</th>
-    //         <th scope="col">MSSV</th>
-    //         <th scope="col">Địa chỉ</th>
-    //       </tr>
-    //     </thead>
-    //     <tbody>
-    //       <StudentList students={students} />
-    //     </tbody>
-    //   </table>
-    // </div>
-
-  );
+  )
 }
+
 export default App;
-// <div className="container">
-//   <h1>Điểm sinh viên</h1>
-//   <div className='row'>
-//     <button className='btn btn-danger w-auto' onClick={minusPoint}>{`-`}</button>
-//     <h1 className='text-primary w-auto'>{point}</h1>
-//     <button className='btn btn-primary w-auto' onClick={plusPoint}>{`+`}</button>
-//     <h1 className='text-danger'>{point >=5 ? 'pass' : 'fail'}</h1>
-//   </div>
-
-//   <div className='row'>
-//     <button className='btn btn-danger w-auto' onClick={minus}>{`-`}</button>
-//     <button className='btn btn-danger w-auto' onClick={setCount.bind(this, count -1)}>{`-`}</button>
-//     {/* chỉ cần làm một chức năng duy nhất không có nhu cầu sử dụng lại {setCount.bind(this, count -1)}*/}
-//     <h1 className='text-primary w-auto'>{count}</h1>
-//     <button className='btn btn-primary w-auto' onClick={plus}>{`+`}</button>
-//   </div>
-//   {students?.map((value, index)=>{
-//     return <StudentInfo1 key = {index}
-//     no = {index+1}
-//     code = {value?.code}
-//     name = {value?.name}
-//     address= {value?.address?.city || value?.address || '-'} />
-//   })}
-// </div>
-
-
-
--- test/test_1/src/pages/Login/index.js
+++ test/test_1/src/pages/Login/index.js
@@ -1,13 +1,110 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useState } from 'react';
 
-function Login() {
+
+const Login = () => {
+    // const [username, setUsername] = useState('');
+    // const [password, setPassword] = useState('');
+    // const usernameChange = (props) => {
+    //     console.log("on username change === ", props.target.value);
+    //     setUsername(props.target.value);
+    // }
+    // const passwordChange = (props) => {
+    //     console.log("on password change === ", props.target.value);
+    //     setPassword(props.target.value);
+    // }
+
+    // const onTextChange = (props) => {
+    //     console.log("name === ", props.target.name);
+    //     console.log("value === ", props.target.value);
+    //     if(props.target.name === 'username') {
+    //         setUsername(props.target.value);
+    //         return;
+    //     }
+    //     if(props.target.name === 'password') {
+    //         setPassword(props.target.value);
+    //         return;
+    //     }   
+    // }
+
+    const [errors, setErrors] = useState({
+
+    });
+    const [value, setValue] = useState({
+        username: '',
+        password: '',
+        confirmPassword: ''
+    });
+
+    const onTextChange = (props) => {
+        console.log("name === ", props.target.name);
+        console.log("value === ", props.target.value);
+        setValue({
+            ...value,
+            [props.target.name]: props.target.value
+        })
+    }
+
+    const login = () => {
+        let errorsTemp = {};
+        Object.keys(value).map((item, index) => {
+            if (value[item]?.trim() === '') {
+                errorsTemp = {
+                    ...errorsTemp,
+                    [item]: `${item} không được để trống`
+                };
+            }
+        });
+        setErrors(errorsTemp);
+
+    }
+
+    useEffect(() => {
+        console.log("value === ", value);
+    }, [value])
+
+
+
     return (
         <div className='container'>
-           <h1>Login</h1>
-           <a className='btn btn-primary' href='/login'>Đăng nhập</a>
-           <a className='btn btn-primary' href='/register'>Đăng kí</a>
+            <div className='container'>
+                <h1>Login</h1>
+                <a className='btn btn-primary' href='/login'>Đăng nhập</a>
+                <a className='btn btn-primary' href='/register'>Đăng kí</a>
+            </div>
+            <div className='col-4 offset-4'>
+                <h1 className='text-primary'>Login</h1>
+                <div className='mb-3'>
+                    <label className='form-label'>Username</label>
+                    <input name='username' type='text' value={value.username} className='form-control' onChange={onTextChange} />
+                    {errors.username && <small className='text-danger'>{errors.username}</small>}
+                </div>
+                <div className='mb-3'>
+                    <label className='form-label'>Password</label>
+                    <input name='password' type='password' value={value.password} className='form-control' onChange={onTextChange} />
+                    {errors.password && <small className='text-danger'>{errors.password}</small>}
+                </div>
+                <div className='mb-3'>
+                    <label className='form-label'>Confirm Password</label>
+                    <input name='confirmPassword' type='password' value={value.confirmPassword} className='form-control' onChange={onTextChange} />
+                    {errors.confirmPassword && <small className='text-danger'>{errors.confirmPassword}</small>}
+                </div>
+                <button className='btn btn-success w-100 mb-3' onClick={login} >{`Login`}</button>
+                <a className='btn btn-primary w-auto me-3' href='/register'>Register</a>
+            </div>
         </div>
-    );
+    )
 }
+
+
+// function Login() {
+//     return (
+//         <div className='container'>
+//            <h1>Login</h1>
+//            <a className='btn btn-primary' href='/login'>Đăng nhập</a>
+//            <a className='btn btn-primary' href='/register'>Đăng kí</a>
+//         </div>
+//     );
+// }
 
 export default Login;
